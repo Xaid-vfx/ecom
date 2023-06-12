91ff8a2228c8123f3d229652f0ea211ff53c3f84
Adi-070 <mishraaditya760@gmail.com>
2023-06-12 17:01:03 +0530

    home-basic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import Navbar from './components/navbar/navbar'
+import Home from './pages/home/home';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import React from 'react';
 
 function App() {
   return (
     
     <div className="App">
       <Navbar />
-      This is your project
+
+      <p>this is our project</p>
+
+      <Router>
+
+        <Routes>
+
+          <Route path="/"element={<Home/>} />
+
+        </Routes>
+        
+      </Router> 
+
     </div>
   );
 }
---
+++ src/pages/home/home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button } from '@mui/material-next'
+import "./home.css"
+
+
+
+const Home = () => {
+  return (
+    <section>
+    <div className="banner" style={{
+      backgroundSize:"contain",backgroundPosition:"center center", height:"600px"
+    }}>
+     
+
+   
+   
+    <div className='button-container' style={{bottom:"100px", padding:"10px", position:"absolute"}}>
+    <Button variant='contained' color="primary"size="large" sx = {{backgroundColor:"black", color:"gray", fontWeight:"600", fontFamily:"Verdana, Geneva, Tahoma, sans-serif"}}>learn more</Button>
+    </div>
+    
+
+    </div>
+    </section>
+  )
+}
+
+export default Home
